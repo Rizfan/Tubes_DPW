9f961d0a0031e5c801043d39a270cbccdc7b8dcb
Rizfan <rispanherlaya@gmail.com>
2023-07-25 20:33:22 +0700

    produk

--- layout/navbar.php
+++ layout/navbar.php
@@ -13,24 +13,24 @@
 
     <?php if (str_contains($tittle, "Dashboard")) { ?>
         <!-- Custom fonts for this template-->
-        <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+        <link href="../assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
         <!-- Custom styles for this template-->
-        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
 
-        <link href="assets/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet" />
+        <link href="../assets/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet" />
 
     <?php } else { ?>
 
         <!-- Custom fonts for this template-->
-        <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+        <link href="../assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
         <!-- Custom styles for this template-->
-        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
 
-        <link href="assets/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet" />
+        <link href="../assets/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet" />
     <?php } ?>
 </head>
 
