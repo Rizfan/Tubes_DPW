417e3d66daee39f1e00322a0d713962409312d0d
sandrocods <59155826+sandrocods@users.noreply.github.com>
2023-07-25 15:42:37 +0700

    update

---
+++ src/proses/proses_session.php
@@ -0,0 +1,58 @@
+<?php
+
+function session_manager($type = "add_session", $data_session = null)
+{
+    session_start();
+    if (strtolower($type) == "add_session") {
+
+        for ($i = 0; $i < count($data_session); $i++) {
+            $_SESSION[$data_session[$i][0]] = $data_session[$i][1];
+        }
+
+    } else if (strtolower($type) == "destroy_session") {
+        session_destroy();
+
+    } else if (strtolower($type) == "get_session") {
+
+        $data_sessione = [];
+
+        for ($i = 0; $i < count($data_session); $i++) {
+            if (!isset($_SESSION[$data_session[$i]])) {
+                $data_sessione[$data_session[$i]] = null;
+            } else {
+                $data_sessione[$data_session[$i]] = $_SESSION[$data_session[$i]];
+
+            }
+
+        }
+        return $data_sessione;
+
+    }
+
+}
+
+function redirect_to_role_page()
+{
+    $get_current_session = session_manager("get_session", ["username", "role"]);
+    if ($get_current_session['username'] != null) {
+
+        // Cek role
+        if ($get_current_session['role'] != null) {
+
+            // Redirect ke halaman sesuai role
+            header("Location: ./" . strtolower($get_current_session['role']) . "/");
+            exit();
+
+        } else {
+
+            echo "Role not found";
+            exit();
+        }
+
+    } else {
+
+        // Redirect ke halaman landing karena belum login
+        header("Location: " . $_SERVER['WEB_ROOT'] . "landing/");
+        exit();
+    }
+}
