bd41d2409d515802a9f4d2ae017d0a890864c0eb
Rizfan <rispanherlaya@gmail.com>
2023-07-26 22:45:21 +0700

    dashboard

--- penjual/index.php
+++ penjual/index.php
@@ -19,7 +19,7 @@ include '../src/database/dashboard.php';
     <div class="row">
 
         <!-- Earnings (Monthly) Card Example -->
-        <!-- <div class="col-xl-3 col-md-6 mb-4">
+        <div class="col-xl-3 col-md-6 mb-4">
             <div class="card border-left-primary shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
@@ -34,7 +34,7 @@ include '../src/database/dashboard.php';
                     </div>
                 </div>
             </div>
-        </div> -->
+        </div>
 
         <!-- Earnings (Monthly) Card Example -->
         <div class="col-xl-3 col-md-6 mb-4">
