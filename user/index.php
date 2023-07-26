ca0f343f5a07c3c2537aff046edd4a2a966af081
Rizfan <rispanherlaya@gmail.com>
2023-07-26 22:37:09 +0700

    dashboard

--- user/index.php
+++ user/index.php
@@ -1,9 +1,9 @@
 <?php
 require_once './../src/proses/proses_session.php';
-if (session_manager("get_session", ['username', 'role'])['role'] != "User") {
-    redirect_to_role_page("http://localhost/Tubes_DPW/landing/");
+if (session_manager("get_session", ['username', 'id_penjual'])['id_penjual'] != NULL) {
+    redirect_to_role_page("http://localhost/Tubes_DPW/penjual/index.php");
 }
-// var_dump(session_manager("get_session", ['username', 'role']));
+$id_user = session_manager("get_session", ['id_user'])['id_user'];
 
 $tittle = 'Dashboard';
 require_once './../src/database/dashboard.php';
@@ -16,41 +16,6 @@ include '../layout/master_dashboard.php';
     <!-- Content Row -->
     <div class="row">
 
-        <!-- Earnings (Monthly) Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-primary shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                User</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php count_users() ?></div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-users fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Earnings (Monthly) Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-success shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                Toko</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php count_penjual() ?></div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-store fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
 
         <!-- Earnings (Monthly) Card Example -->
         <div class="col-xl-3 col-md-6 mb-4">
@@ -58,10 +23,10 @@ include '../layout/master_dashboard.php';
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Transaksi
+                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Riwayat Transaksi
                             </div>
 
-                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php count_transaksi() ?></div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php count_riwayat_transaksi($id_user) ?></div>
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
@@ -71,23 +36,6 @@ include '../layout/master_dashboard.php';
             </div>
         </div>
 
-        <!-- Pending Requests Card Example -->
-        <!-- <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-warning shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                Pending Requests</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-comments fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div> -->
     </div>
 </section>
 
